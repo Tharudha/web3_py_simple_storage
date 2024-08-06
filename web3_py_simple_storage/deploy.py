54eea7b9b4822d474c325c80945b417f2aa6b370
Tharudha <106004111+Tharudha@users.noreply.github.com>
2024-08-06 12:06:31 +0530

    Add files via upload

---
+++ web3_py_simple_storage/deploy.py
@@ -0,0 +1,101 @@
+from solcx import compile_standard, install_solc 
+install_solc("0.6.12")
+import json
+from web3 import Web3 
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.12",
+)
+
+#To deploy u need to get byte code and ABI
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+# For connecting to ganache
+w3 = Web3(Web3.HTTPProvider("https://sepolia.infura.io/v3/e44abab1e74b4090a137608da2e25f89"))
+chain_id = 11155111
+my_address = "0xffdCC5a28818466C6F4bFf6050B8691307722A1c"
+private_key = os.getenv("PRIVATE_KEY")
+# print(os.getenv("PRIVATE_KEY"))
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.get_transaction_count(my_address)
+# print(nonce)
+
+#1. build transaction
+#2. sign transaction
+#3. send transaction
+transaction = SimpleStorage.constructor().build_transaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# print(transaction)
+
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+#We do call the retrive function because no state change we need
+#initial value of the fav number
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+
+greeting_transaction = simple_storage.functions.store(15).build_transaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+print("Updated")
+print(simple_storage.functions.retrieve().call())
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
